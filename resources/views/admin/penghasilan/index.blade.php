b4c0794a85e9b0bff94f165e18f2f8fd8b653db6
ZakyMalika <zakymalika0987@gmail.com>
2025-11-11 13:36:11 +0700

    menambah pagination

--- resources/views/admin/penghasilan/index.blade.php
+++ resources/views/admin/penghasilan/index.blade.php
@@ -96,6 +96,16 @@
                     </tbody>
                 </table>
             </div>
+            @if($users->hasPages())
+                    <div class="d-flex justify-content-between align-items-center mt-3">
+                        <div class="text-muted">
+                            Menampilkan {{ $users->firstItem() ?? 0 }} sampai {{ $users->lastItem() ?? 0 }} dari {{ $users->total() }} data
+                        </div>
+                        <div>
+                            {{ $users->links('pagination::bootstrap-4') }}
+                        </div>
+                    </div>
+                @endif
         </div>
     </div>
 </div>
