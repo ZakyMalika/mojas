7f6696c1e7689dba7658bbf3a92afa00a601243b
ZakyMalika <zakymalika0987@gmail.com>
2026-05-21 10:38:44 +0700

    menghilangkan 1 fungsi paginasi

--- resources/views/admin/penghasilan/index.blade.php
+++ resources/views/admin/penghasilan/index.blade.php
@@ -211,7 +211,7 @@
                                 <option value="25" {{ request('per_page') == '25' ? 'selected' : '' }}>25</option>
                                 <option value="50" {{ request('per_page') == '50' ? 'selected' : '' }}>50</option>
                                 <option value="100" {{ request('per_page') == '100' ? 'selected' : '' }}>100</option>
-                                <option value="all" {{ request('per_page') == 'all' ? 'selected' : '' }}>Semua</option>
+                                {{-- <option value="all" {{ request('per_page') == 'all' ? 'selected' : '' }}>Semua</option> --}}
                             </select>
                             <small class="text-muted">data</small>
                         </form>
